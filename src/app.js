c7cb3c34230983049b01ee43ad5721e85e3f7a8e
Yura Lapitski <lyurik@tut.by>
2021-12-05 21:33:59 +0300

    feat: add swagger and edit package.json

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 const fastify = require('fastify')({ logger: true });
+const swaggerUI = require('fastify-swagger');
+const path = require('path');
+
+fastify.register(swaggerUI, {
+  mode: 'static',
+  specification: {
+    path: path.join(__dirname, '../doc/api.yaml'),
+  },
+  exposeRoute: true,
+  routePrefix: '/doc',
+});
 
 fastify.register(require('./resources/users/user.router'), {
   prefix: '/users',
