9937b555e205fbcf716eaf6c29f7dbb43ddaa8be
Yura Lapitski <lyurik@tut.by>
2021-12-23 15:21:43 +0300

    feat: add fastify logger

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,20 @@
 import fastify from 'fastify';
 import swaggerUI from 'fastify-swagger';
-// import pino from 'pino';
-// import loggerPino from './logger';
-// import winston from 'winston';
-// eslint-disable-next-line node/no-missing-import
-// import morgan from 'morgan';
-// import middie from 'middie';
-// import fastifyExpress from  "fastify-express";
-import middie from 'middie';
-// eslint-disable-next-line node/no-missing-import
-import morgan from 'morgan';
+import { logger } from './logger';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
-// import  config  from './common/config';
+
 
 // logger: logger as unknown as FastifyLoggerInstance,++++++++++++++++++++++++++++++++
-const server =  fastify({ logger: false });
-  // logger: false
-// {
-//     level: config.LEVEL_LOG,
-//     file: 'log.log',
-//     serializers: {
-//       res(reply) {
-//         return {
-//           statusCode: reply.statusCode
-//         }
-//       },
-//       req(request) {
-//         return {
-//           method: request.method,
-//           url: request.url,
-//           query: request.query,
-//           parameters: request.params,
-//           body: request.body,
-//         }
-//       }
-//     }
-//   } 
-//    }
-// );
-
-// server.addHook('preHandler', (req, _reply, done) => {
-//   if (req.body) {
-//     req.log.info({ body: req.body }, 'parsed body')
-//   }
-//   done()
-// })
-
-// server.register(fastifyExpress)
-server.register(middie);
-// // fastify.use(require('cors')())
-server.use(morgan('dev'));
-
+const server =  fastify({ logger} );
+  
+server.addHook('preHandler', (req, _reply, done) => {
+  if (req.body) {
+    req.log.info({ body: req.body }, 'parsed body')
+  }
+  done()
+})
 
 server.register(swaggerUI, {
   exposeRoute: true,
@@ -65,8 +27,6 @@ server.register(swaggerUI, {
     },
   },
 });
-
-// server.register(morgan('dev'));
 
 server.register(userRouter, {
   prefix: '/users',
--- src/logger.ts
+++ src/logger.ts
@@ -1,20 +1,42 @@
-import pino from 'pino';
-import path from 'path';
+import { FastifyReply, FastifyRequest } from "fastify";
+import  config  from './common/config';
 
-const transport = pino.transport({
-    targets: [
+
+export const logger = {
+    level: config.LEVEL_LOG,
+    file: 'log.log',
+    serializers: {
+      res(reply: FastifyReply) {
+        return {
+          statusCode: reply.statusCode
+        }
+      },
+      req(request: FastifyRequest) {
+        return {
+          method: request.method,
+          url: request.url,
+          query: request.query,
+          parameters: request.params,
+          body: request.body,
+        }
+      }
+    }
+  } 
+
+// const transport = pino.transport({
+//     targets: [
+// //         {
+// //             level: 'info',
+// //             target: 'pino/file',
+// //   options: { destination: '/log.log', mkdir: true, append: true }
+// //         }, 
 //         {
 //             level: 'info',
-//             target: 'pino/file',
-//   options: { destination: '/log.log', mkdir: true, append: true }
-//         }, 
-        {
-            level: 'info',
-            target: path.resolve(__dirname,'../log.log'),
-            options: {
-            }
-        }
-    ]
-});
+//             target: path.resolve(__dirname,'../log.log'),
+//             options: {
+//             }
+//         }
+//     ]
+// });
 
-export default pino(transport);
+// export default pino(transport);
