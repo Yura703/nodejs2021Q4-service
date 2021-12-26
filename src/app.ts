fad7f5996881e70685311a6fda6526d2932c835d
Yura Lapitski <lyurik@tut.by>
2021-12-26 20:40:03 +0300

    feat: refactoring app.ts and logger.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,56 @@
 import fastify from 'fastify';
 import swaggerUI from 'fastify-swagger';
-import { pinoLogger } from './logger';
+// import { pinoLogger } from './logger';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import  config  from './common/config';
 
+const server =  fastify({ 
+  disableRequestLogging: true,
+  logger: {
+    level: config.LEVEL_LOG,
+    file: 'log.log',
+    serializers: {
+      res(reply) {
+        return {
+          statusCode: reply.statusCode
+        }
+      },
+      req(request) {
+        return {
+          method: request.method,
+          url: request.url,
+          query: request.query,
+          parameters: request.params,
+          body: request.body,
+        }
+      }
+    },
+    prettyPrint: {
+      ignore: 'log,pid,time,hostname,req.headers,reqId,',
+      colorize: false,
+    },
+    // serializers: {
+    //   res(reply: FastifyReply) {
+    //     return {
+    //       statusCode: reply.statusCode,
+    //     };
+    //   },
+    //   req(request: FastifyRequest) {
+    //     return {
+    //       method: request.method,
+    //       url: request.url,
+    //       path: request.routerPath,
+    //       parameters: request.params,
+    //       body: request.body,
+    //     };
+    //   },
+    // },
+  },
+});
 
-// logger: logger as unknown as FastifyLoggerInstance,++++++++++++++++++++++++++++++++
-const server =  fastify({ logger: pinoLogger} );
+// pinoLogger(app, errorLogFilePath);
   
 server.addHook('preHandler', (req, _reply, done) => {
   if (req.body) {
@@ -42,5 +85,18 @@ server.register(boardRouter, {
 server.register(taskRouter, {
   prefix: '/boards/:boardId/tasks',
 });
+
+// //const logger = new Logger({ app });
+
+// process.on('uncaughtException', (reason) => {
+//   log.error(`CAPTURED ERROR:${reason.message}`);
+//   process.exit(1);
+// });
+// // throw Error('Oops!');
+
+// process.on('unhandledRejection', (reason: Error) => {
+//   logger.error(`UNHANDLED REJECTION DETECTED: ${reason.message}`);
+//   process.exit(1);
+// });
 
 export = server;
