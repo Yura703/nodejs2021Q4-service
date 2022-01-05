f7a4e5799bd702a20c70324414186ec43abd4c15
Yura Lapitski <lyurik@tut.by>
2022-01-05 16:16:21 +0300

    feat: add docker-compose

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const PORT =  config.PORT ?? 4000;
  */
 const start = async () => {
   try {
-    await fastify.listen(PORT);
+    await fastify.listen(PORT, "0.0.0.0");
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
