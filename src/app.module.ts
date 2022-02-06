1ed491b6968d3f1929c13858bb1854ac5e2cfc05
Юрий Лапицкий <C:\Users\lyuri\AppData\Roaming\The Bat!>
2022-02-06 21:42:18 +0300

    feat: edit migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,10 @@ import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
 import { FilesModule } from './files/files.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-//import ormConfig from '../ormConfig';
 import { User } from './users/entities/user.entity';
 import { Task } from './tasks/entities/task.entity';
 import { Board } from './boards/entities/board.entity';
-//import { DBModule } from './db.module';
-//import { AuthModule } from './auth/auth.module';
 import { LoginModule } from './login/login.module';
-//import { join } from 'path';
 
 @Module({
   imports: [
@@ -22,24 +18,9 @@ import { LoginModule } from './login/login.module';
     UsersModule,
     BoardsModule,
     FilesModule,
-    //DBModule,
     ConfigModule.forRoot({
       isGlobal: true,
-    }), //добавить все модули
-    // TypeOrmModule.forRoot({
-    //   ...ormConfig,
-    //   autoLoadEntities: true,
-    // }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'localhost',
-    //   port: 5434,
-    //   username: 'postgres',
-    //   password: 'pass',
-    //   database: 'test',
-    //   entities: [User, Task, Board],
-    //   synchronize: true,
-    // }),
+    }), 
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
@@ -47,22 +28,14 @@ import { LoginModule } from './login/login.module';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      //entities: [join(__dirname, '**', '*.entity.{ts,js}')],
       entities: [User, Task, Board],
       synchronize: false,
-      migrations: ['dist/src/migrations/*{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
       migrationsRun: true,
-      // cli: {
-      //   migrationsDir: 'src/migrations',
-      // },
-
-      //"entities": ["dist/**/*.entity{ .ts,.js}"],
-
-      //migrations: ['dist/migrations/*{.ts,.js}'],
-      //migrationsTableName: 'migrations_typeorm',
-      //migrationsRun: true,
+      cli: {
+        migrationsDir: 'src/migrations',
+      },
     }),
-    //AuthModule,
     LoginModule,
   ],
   controllers: [AppController],
